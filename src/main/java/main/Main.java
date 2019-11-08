eb718e2d0fb93a96fb2ec2682e2a3276cba7eb04
Riina-Alisa Huotari <riina-alisa.huotari@helsinki.fi>
2019-11-08 09:37:29 +0200

    Viikko2 tehtävä 3

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,14 +3,17 @@ package ohtu.ohtuvarasto;
 public class Main {
 
     public static void main(String[] args) {
-
+        
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
 
         System.out.println("Luonnin jälkeen:");
         System.out.println("Mehuvarasto: " + mehua);
         System.out.println("Olutvarasto: " + olutta);
+    }
+}
 
+/*
         System.out.println("Olutgetterit:");
         System.out.println("getSaldo()     = " + olutta.getSaldo());
         System.out.println("getTilavuus    = " + olutta.getTilavuus());
@@ -54,5 +57,4 @@ public class Main {
         saatiin = mehua.otaVarastosta(-32.9);
         System.out.println("saatiin " + saatiin);
         System.out.println("Mehuvarasto: " + mehua);
-    }
-}
+        */
